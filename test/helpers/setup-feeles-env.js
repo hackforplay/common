2c59dbbe0deb0d77dc0d3efcb8f54c45335175e4
teramotodaiki <i101323@gmail.com>
2018-09-03 19:04:50 +0900

    test: feeles.fetchDataURL のモック

--- test/helpers/setup-feeles-env.js
+++ test/helpers/setup-feeles-env.js
@@ -1,6 +1,21 @@
 // https://github.com/Feeles/IDE/blob/master/src/Cards/MonitorCard/registerHTML.js#L33
 if (!global.feeles) {
     global.feeles = {
-        env: {}
+        env: {},
+        fetchDataURL
     }
+}
+
+/**
+ * Feeles 内のファイルを Data URL 形式で取得する
+ * @param {String} src ソースファイルのパス
+ */
+function fetchDataURL(src) {
+    return new Promise((resolve, reject) => {
+        const filePath = require('path').resolve(__dirname, '../../src/', src);
+        require('fs').readFile(filePath, 'base64', (err, data) => {
+            if (err) reject(err);
+            else resolve(data);
+        });
+    });
 }
