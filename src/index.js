8c939c0e04e2924ae98cbc9dbba520533eca5e4f
teramotodaiki <i101323@gmail.com>
2018-09-03 19:04:26 +0900

    feature: common のエンドポイントから enchant, Hack をエクスポート

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import enchant from './enchantjs/enchant';
 import './hackforplay/core';
+import Hack from './hackforplay/hack';
+import './hackforplay/enchantjs-kit'; // Core の生成を待つ
+
+export {
+    Hack,
+    enchant
+};
