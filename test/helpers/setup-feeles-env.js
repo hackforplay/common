5a0f5febc5fad998b57316f86141e8b4dd615d7f
teramotodaiki <i101323@gmail.com>
2018-09-04 15:15:04 +0900

    test: mock feeles.hogehoge functions

--- test/helpers/setup-feeles-env.js
+++ test/helpers/setup-feeles-env.js
@@ -1,14 +1,39 @@
-import register from './timers';
+import register from "./timers";
 
 // https://github.com/Feeles/IDE/blob/master/src/Cards/MonitorCard/registerHTML.js#L33
 if (!global.feeles) {
-    global.feeles = {
-        env: {},
-        fetchDataURL,
-        throwError,
-        connected: new Promise(() => {})
-    }
-    register(global.feeles);
+  global.feeles = {
+    env: {},
+    fetchDataURL,
+    throwError,
+    connected: new Promise(() => {}),
+    exports: () => {},
+    fetch: () => {},
+    resolve: () => {},
+    saveAs: () => {},
+    reload: () => {},
+    replace: () => {},
+    openReadme: () => {},
+    closeReadme: () => {},
+    openMedia: () => {},
+    closeMedia: () => {},
+    openCode: () => {},
+    closeCode: () => {},
+    openEditor: () => {},
+    closeEditor: () => {},
+    setAlias: () => {},
+    runCode: () => {},
+    SpeechRecognition: () => {},
+    ipcRenderer: () => {},
+    setTimeout: () => {},
+    clearTimeout: () => {},
+    setInterval: () => {},
+    clearInterval: () => {},
+    dispatchOnMessage: () => {},
+    openWindow: () => {},
+    eval: () => {}
+  };
+  register(global.feeles);
 }
 
 /**
@@ -16,23 +41,23 @@ if (!global.feeles) {
  * @param {String} src ソースファイルのパス
  */
 function fetchDataURL(src) {
-    return new Promise((resolve, reject) => {
-        const filePath = require('path').resolve(__dirname, '../../src/', src);
-        const ext = require('path').extname(src);
-        const mime = {
-            '.png': 'image/png',
-            '.gif': 'image/gif'
-        }[ext];
-        require('fs').readFile(filePath, 'base64', (err, data) => {
-            if (err) reject(err);
-            else {
-                const dataURL = `data:${mime};base64,${data}`;
-                resolve(dataURL);
-            }
-        });
+  return new Promise((resolve, reject) => {
+    const filePath = require("path").resolve(__dirname, "../../src/", src);
+    const ext = require("path").extname(src);
+    const mime = {
+      ".png": "image/png",
+      ".gif": "image/gif"
+    }[ext];
+    require("fs").readFile(filePath, "base64", (err, data) => {
+      if (err) reject(err);
+      else {
+        const dataURL = `data:${mime};base64,${data}`;
+        resolve(dataURL);
+      }
     });
+  });
 }
 
 function throwError(error) {
-    throw error;
+  throw error;
 }
