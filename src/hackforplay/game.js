39079ab4682214f78394c23e962b8186ccd82301
teramotodaiki <i101323@gmail.com>
2018-09-05 18:52:33 +0900

    fix: TypeError: _cache.has is not a function
    #10

--- src/hackforplay/game.js
+++ src/hackforplay/game.js
@@ -1,8 +1,10 @@
 import enchant from '../enchantjs/enchant';
 import '../enchantjs/ui.enchant';
 
-// すべてのenchantモジュールをグローバルにエクスポート
-enchant('ui');
+// https://github.com/hackforplay/common/issues/10
+const _Map = window.Map;
+enchant('ui'); // すべてのenchantモジュールをグローバルにエクスポート
+window.Map = _Map;
 
 // コアのインスタンスを生成
 const game = new enchant.Core(480, 320);
