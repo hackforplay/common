5a8ade524c544672e84c5b7c358babf41e1d571b
teramotodaiki <i101323@gmail.com>
2018-12-27 11:01:31 +0900

    feature: Dir ベクトルの型を Dir にする

--- src/hackforplay/dir.ts
+++ src/hackforplay/dir.ts
@@ -4,22 +4,11 @@ export interface Dir {
   (): Vector2;
 }
 
-export function up() {
-  return new Vector2(0, -1);
-}
-export function right() {
-  return new Vector2(1, 0);
-}
+export const up: Dir = () => new Vector2(0, -1);
+export const right: Dir = () => new Vector2(1, 0);
+export const down: Dir = () => new Vector2(0, 1);
+export const left: Dir = () => new Vector2(-1, 0);
 
-export function down() {
-  return new Vector2(0, 1);
-}
-
-export function left() {
-  return new Vector2(-1, 0);
-}
-
-export const random = random4;
 const m = -1;
 
 /**
@@ -29,10 +18,11 @@ const m = -1;
  */
 const _xRandom4 = [0, 1, 0, m];
 const _yRandom4 = [m, 0, 1, 0];
-export function random4() {
+export const random4: Dir = () => {
   const n = (Math.random() * 4) >> 0;
   return new Vector2(_xRandom4[n], _yRandom4[n]);
-}
+};
+export const random = random4;
 
 /**
  * 0 1 2
@@ -41,10 +31,10 @@ export function random4() {
  */
 const _xRandom8 = [m, 0, 1, 1, 1, 0, m, m];
 const _yRandom8 = [m, m, m, 0, 1, 1, 1, 0];
-export function random8() {
+export const random8: Dir = () => {
   const n = (Math.random() * 8) >> 0;
   return new Vector2(_xRandom8[n], _yRandom8[n]);
-}
+};
 
 // synonyms
 export const うえ = up;
