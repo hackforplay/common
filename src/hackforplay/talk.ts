e9374d82e1ca5e725e7b4609215456507f8b9a5a
RyoTaniguchi <rx.kino.kinoko@gmail.com>
2019-02-24 00:25:17 +0900

    配列answerにtextareaを入れるように。インデックス番号を渡すのを止めた

--- src/hackforplay/talk.ts
+++ src/hackforplay/talk.ts
@@ -1,10 +1,7 @@
 import { default as Hack } from './hack';
 import TextArea from '../hackforplay/ui/textarea';
-import game from '../hackforplay/game';
-import { resolve } from 'url';
 
 var answers: any = [];
-var choicesNum: number = 0; // 選択肢の数
 
 // テキストエリアを生成
 const textArea = new TextArea(480, 200);
@@ -26,33 +23,41 @@ textArea.defaultStyle = {
   rubyId: null
 };
 
-const MakeAnswers = function(i: number) {
-  Hack.menuGroup.addChild(answers[i]); // メニューにaddChild
-  answers[i].x = 480 - answers[i].w;
-  answers[i].y = 320 - textArea.height - answers[i].height * (i + 1);
-  answers[i].margin = 2;
-  answers[i].padding = 5;
-  answers[i].borderRadius = 14;
-  answers[i].borderColor = '#fff';
-  answers[i].borderWidth = 2;
-  answers[i].defaultStyle = {
+const makeAnswer = function(choice: string, resolve: (text: string) => void) {
+  const textWindow = new TextArea(180, 32);
+  Hack.menuGroup.addChild(textWindow); // メニューにaddChild
+  textWindow.x = 480 - textWindow.w;
+  textWindow.y =
+    320 - textArea.height - textWindow.height * (answers.length + 1);
+  textWindow.margin = 2;
+  textWindow.padding = 5;
+  textWindow.borderRadius = 14;
+  textWindow.borderColor = '#fff';
+  textWindow.borderWidth = 2;
+  textWindow.defaultStyle = {
     color: '#fff',
     size: '16',
     family: 'PixelMplus, sans-serif',
     weight: 'bold',
     align: 'center',
+    lineSpace: 0,
     space: 0,
     ruby: null,
     rubyId: null
   };
-  answers[i].clear(); // 前の文章をクリア
-  answers[i].show();
+  textWindow.clear(); // 前の文章をクリア
+  textWindow.show();
+  textWindow.push(choice); // 選択肢のテキスト表示
+  textWindow.on('touchend', function() {
+    windowDelete();
+    resolve(choice);
+  });
+  return textWindow;
 };
 
 export default function talk(text: string, ...choices: string[]) {
   return new Promise(resolve => {
     if (textArea.visible) windowDelete(); // 表示されてるウィンドウを消す
-    choicesNum = choices.length - 1;
     choices.reverse(); // 下から上に表示するので、選択肢の配列をリバースする
     // 本文のテキストエリア作成
     Hack.menuGroup.addChild(textArea);
@@ -60,40 +65,24 @@ export default function talk(text: string, ...choices: string[]) {
     textArea.clear(); // 前の文章をクリア
     textArea.push(text); // テキストを挿入
     textArea.y = 320 - textArea.height;
-    // 選択肢のボタンを作成
     if (choices.length === 0) {
-      choicesNum = 0;
-      const textWindow = new TextArea(68, 32);
-      answers.push(textWindow);
-      MakeAnswers(choicesNum);
-      answers[choicesNum].push('とじる'); // とじる 表示
-      answers[choicesNum].on('touchend', function() {
-        windowDelete();
-        resolve('とじる');
-      });
-    } else {
-      for (var i = 0; i <= choicesNum; i++) {
-        const textWindow = new TextArea(180, 32);
-        answers.push(textWindow);
-        const choice = choices[i];
-        MakeAnswers(i);
-        answers[i].push(choice); // 選択肢のテキスト表示
-        answers[i].on('touchend', function() {
-          windowDelete();
-          resolve(choice);
-        });
-      }
+      choices.push('とじる'); // 選択肢のテキスト表示
+    }
+    // 選択肢のボタンを作成
+    for (var choice of choices) {
+      const answer = makeAnswer(choice, resolve);
+      answers.push(answer);
     }
   });
 }
 
-export function windowDelete() {
+const windowDelete = function() {
   if (!textArea.visible) return;
   Hack.menuGroup.removeChild(textArea);
   textArea.hide();
-  for (var i = 0; i <= choicesNum; i++) {
-    Hack.menuGroup.removeChild(answers[i]);
-    answers[i].hide();
+  for (var answer of answers) {
+    Hack.menuGroup.removeChild(answer);
+    answer.hide();
   }
   answers = [];
-}
+};
