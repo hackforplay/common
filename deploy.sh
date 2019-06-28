60d7e52898a3dd031e8dac270a54307349b429fd
teramotodaiki <i101323@gmail.com>
2019-06-29 00:01:49 +0900

    chore: subdirectory like 0.1.2 and 0.1

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,7 @@ set -e
 echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
-gsutil cp -r dist gs://cdn.hackforplay.xyz/common/test
+PATCH_VERSION=${TRAVIS_TAG:1}
+MINOR_VERSION=${PATCH_VERSION%.*}
+gsutil cp -r dist gs://cdn.hackforplay.xyz/common/${PATCH_VERSION}
+gsutil cp -r gs://cdn.hackforplay.xyz/common/${PATCH_VERSION} gs://cdn.hackforplay.xyz/common/${MINOR_VERSION}
