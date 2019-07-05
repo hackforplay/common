4990cf4a38bf427b9295b63b77ce662120f9cf91
teramotodaiki <i101323@gmail.com>
2019-07-05 23:33:44 +0900

    fix: BehaviorTypes.Init を追加

--- src/hackforplay/behavior-types.js
+++ src/hackforplay/behavior-types.js
@@ -1,5 +1,6 @@
 const BehaviorTypes = {
   None: null, // 無状態 (デフォルトではEventは発火されません)[deprecated]
+  Init: 'init', // スキン適用時にアニメーションが未定義だった場合のみ使用される（状態ではない）
   Idle: 'idle', // 立ち状態
   Walk: 'walk', // 歩き状態
   Attack: 'attack', // 攻撃状態
