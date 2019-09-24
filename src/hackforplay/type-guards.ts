c42ae8c16d96e3f3e9c613d357fb8b4c3d0f263e
teramotodaiki <i101323@gmail.com>
2019-09-24 14:41:37 +0900

    chore: remove 0.x

--- src/hackforplay/type-guards.ts
+++
@@ -1,2 +0,0 @@
-export const keys = <T extends {}>(o: T): (keyof T)[] =>
-  Object.keys(o) as (keyof T)[];
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import enchant from './enchantjs/enchant';
-import './enchantjs/fix';
-import './hackforplay/core';
-import Hack from './hackforplay/hack';
-import register from './hackforplay/register';
-import Rule from './hackforplay/rule';
-
-export { Hack, enchant, register, Rule };
