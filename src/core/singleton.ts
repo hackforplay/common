3920ad558f2c3fbf096c4f63145cb3fd1951ca6e
teramotodaiki <i101323@gmail.com>
2019-11-06 22:41:30 +0900

    fix: remove extra

--- src/core/singleton.ts
+++
@@ -1,5 +0,0 @@
-import * as PIXI from 'pixi.js';
-import * as settings from './settings';
-import { skinLoader } from './skin-loader';
-
-export const preloader = new PIXI.Loader(settings.baseUrl).use(skinLoader);
