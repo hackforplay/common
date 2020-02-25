4bedd26dde84be48e6331dea695dd220ba9988ce
teramotodaiki <i101323@gmail.com>
2020-02-25 16:24:00 +0900

    fix: シノニムには値ではなくキー値を与える

--- src/hackforplay/synonyms/direction.ts
+++ src/hackforplay/synonyms/direction.ts
@@ -1,13 +1,13 @@
 import { ISynonyms } from './synonymize';
 
 export const synonyms: ISynonyms = {
-  うえ: 'up',
-  みぎ: 'right',
-  した: 'down',
-  ひだり: 'left',
-  みぎて: 'right hand',
-  ひだりて: 'left hand',
-  うしろ: 'behind',
-  ランダム: 'random',
-  ななめありランダム: 'random diagonal'
+  うえ: 'Up',
+  みぎ: 'Right',
+  した: 'Down',
+  ひだり: 'Left',
+  みぎて: 'RightHand',
+  ひだりて: 'LeftHand',
+  うしろ: 'Behind',
+  ランダム: 'Random',
+  ななめありランダム: 'RandomDiagonal'
 };
