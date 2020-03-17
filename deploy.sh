0eb743b3838e9480aa39a749f6f736da0a024438
teramotodaiki <i101323@gmail.com>
2020-03-17 15:18:52 +0900

    fix: ディレクトリ構造を保持してデプロイする

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 PATCH_VERSION=${TRAVIS_TAG:1}
 MINOR_VERSION=${PATCH_VERSION%.*}
 gsutil cp -r dist gs://cdn.hackforplay.xyz/common/${PATCH_VERSION}
-gsutil cp -r gs://cdn.hackforplay.xyz/common/${PATCH_VERSION}/* gs://cdn.hackforplay.xyz/common/${MINOR_VERSION}
+gsutil cp -r dist gs://cdn.hackforplay.xyz/common/${MINOR_VERSION}
