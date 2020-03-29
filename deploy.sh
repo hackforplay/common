0c57d687019d9686384abedb7b433e7faec3218e
teramotodaiki <i101323@gmail.com>
2020-03-29 19:43:00 +0900

    fix: MINOR_VERSION だけ ** で展開する

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,6 @@ gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
 PATCH_VERSION=${TRAVIS_TAG:1}
 MINOR_VERSION=${PATCH_VERSION%.*}
-echo $PATCH_VERSION # debug
-echo $MINOR_VERSION # debug
+# https://bit.ly/2xzjGVO
 gsutil cp -r dist gs://cdn.hackforplay.xyz/common/${PATCH_VERSION}
-gsutil cp -r dist gs://cdn.hackforplay.xyz/common/${MINOR_VERSION}
+gsutil cp -r dist/** gs://cdn.hackforplay.xyz/common/${MINOR_VERSION}
