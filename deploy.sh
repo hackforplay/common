8fba83f8ba4333dcff3fe319c73d347cc716a9c0
teramotodaiki <i101323@gmail.com>
2020-03-29 18:58:39 +0900

    fix: typo

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
 PATCH_VERSION=${TRAVIS_TAG:1}
 MINOR_VERSION=${PATCH_VERSION%.*}
-echo PATCH_VERSION # debug
-echo MINOR_VERSION # debug
+echo $PATCH_VERSION # debug
+echo $MINOR_VERSION # debug
 gsutil cp -r dist gs://cdn.hackforplay.xyz/common/${PATCH_VERSION}
 gsutil cp -r dist gs://cdn.hackforplay.xyz/common/${MINOR_VERSION}
