cfbb67b2ef0df9bc0e4805cab6f689d014834d74
teramotodaiki <i101323@gmail.com>
2020-04-16 23:29:36 +0900

    fix: Travis CI の設定を削除

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,4 @@
 
 set -e
 
-echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
-gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
-PATCH_VERSION=${TRAVIS_TAG:1}
-MINOR_VERSION=${PATCH_VERSION%.*}
-# https://bit.ly/2xzjGVO
-gsutil cp -r dist gs://cdn.hackforplay.xyz/common/${PATCH_VERSION}
-gsutil cp -r dist/** gs://cdn.hackforplay.xyz/common/${MINOR_VERSION}
