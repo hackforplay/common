59bce803c65a4d48cad929056bf2e5c00d96704b
teramotodaiki <i101323@gmail.com>
2020-05-28 15:31:10 +0900

    fix: frames を調整

--- src/hackforplay/think.ts
+++ src/hackforplay/think.ts
@@ -18,7 +18,7 @@ export const configs: { [key: string]: ThinkImageConfig | undefined } = {
     height: 32,
     dx: 0,
     dy: -8,
-    frames: [0, 4, 1, 4, 2, 4]
+    frames: [0, 3, 1, 3, 2, 50]
   },
   '?': {
     dataUrl: require('../resources/images/question.png').default,
@@ -26,7 +26,7 @@ export const configs: { [key: string]: ThinkImageConfig | undefined } = {
     height: 32,
     dx: 0,
     dy: -8,
-    frames: [0, 4, 1, 4, 2, 4, 3, 4]
+    frames: [0, 3, 1, 3, 2, 4, 3, 50]
   }
 };
 
