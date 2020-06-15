10c90c4a41c2e8851211903e121cd7b4f8d2ecce
teramotodaiki <i101323@gmail.com>
2020-06-15 21:23:09 +0900

    fix: 当たり判定が消えないバグ

--- src/hackforplay/object/object.ts
+++ src/hackforplay/object/object.ts
@@ -547,7 +547,7 @@ export default class RPGObject extends enchant.Sprite implements N.INumbers {
       this.しょうかんする(this.skill);
     } else {
       // ダメージを与えるオブジェクトを生成する
-      damageObject = new RPGObject();
+      damageObject = new RPGObjectWithSynonym(); // eslint-disable-line
       damageObject.damage = this.atk;
       damageObject.collisionFlag = false;
       registerServant(this, damageObject);
