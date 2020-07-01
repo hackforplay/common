e5a2c4f2a47eda33869df2c4f43d3058725ff808
teramotodaiki <i101323@gmail.com>
2020-07-01 15:45:19 +0900

    fix: トラップされる回数は環境によって違うのでテストしない

--- src/hackforplay/utils/observe-array.test.ts
+++ src/hackforplay/utils/observe-array.test.ts
@@ -3,22 +3,27 @@ import { observeArray } from './observe-array';
 
 test('observeArray', t => {
   const a = observeArray([1, 2, 3]);
+  let count = 0;
+  const assertIncreased = () => {
+    t.true(a.mutatedCount > count); // トラップされる回数は環境によって違うのでテストしない. 増えたことだけテストする
+    count = a.mutatedCount;
+  };
   t.is(a.mutatedCount, 0);
   a[3] = 4; // [1, 2, 3, 4]
-  t.is(a.mutatedCount, 1);
+  assertIncreased();
   a.pop(); // [1, 2, 3]
-  t.is(a.mutatedCount, 2);
+  assertIncreased();
   a.push(0, 1); // [1, 2, 3, 0, 1]
-  t.is(a.mutatedCount, 5);
+  assertIncreased();
   a.reverse(); // [1, 0, 3, 2, 1]
-  t.is(a.mutatedCount, 9);
+  assertIncreased();
   a.shift(); // [0, 3, 2, 1]
-  t.is(a.mutatedCount, 14);
+  assertIncreased();
   a.sort(); // [0, 1, 2, 3]
-  t.is(a.mutatedCount, 18);
+  assertIncreased();
   a.splice(1, 1, 99); // [0, 99, 2, 3]
-  t.is(a.mutatedCount, 20);
+  assertIncreased();
   a.unshift(100); // [100, 0, 99, 2, 3]
-  t.is(a.mutatedCount, 26);
+  assertIncreased();
   t.deepEqual(Array.from(a), [100, 0, 99, 2, 3]);
 });
