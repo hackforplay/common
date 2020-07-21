581cc7887d7b09aac7bb5818e12002f4cc45bb81
tenonno <tenonno@gmail.com>
2020-07-21 09:53:55 +0900

    feat: enchant.Surface の代替として SurfaceSprite を実装

---
+++ src/hackforplay/surface-sprite.ts
@@ -0,0 +1,33 @@
+import { Sprite, Texture } from 'pixi.js';
+
+export default class SurfaceSprite extends Sprite {
+  private _element: HTMLCanvasElement;
+
+  public context: CanvasRenderingContext2D;
+
+  public constructor(width: number, height: number) {
+    super();
+
+    this._element = document.createElement('canvas');
+    this._element.width = width;
+    this._element.height = height;
+    this.context = this._element.getContext('2d')!;
+
+    this.texture = Texture.from(this._element);
+    this.resize(width, height);
+  }
+
+  public resize(width: number, height: number) {
+    this._element.width = width;
+    this._element.height = height;
+    this.width = width;
+    this.height = height;
+    this.updateTexture();
+  }
+
+  public updateTexture() {
+    // TODO: 反映されない場合があるので調査する
+    this.texture.baseTexture.update();
+    this.texture.update();
+  }
+}
